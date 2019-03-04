19353d499353eeae6a041cd3767c040e53c54a3e
Reuben Lifshay <rlifshay@gmail.com>
2019-03-04 02:05:15 -0800

    Convert python to buildah.

--- python/cbuild.sh
+++ python/cbuild.sh
@@ -1,54 +1,33 @@
 #!/bin/sh
 set -e
 
-. ${UTILSDIR:=../cbutils/src}/umoci.lib.sh
-. ${UTILSDIR:=../cbutils/src}/runc.lib.sh
-
-u_set_loglevel $U_LOG_INFO
-u_set_autoclean
-
-# create image
-img=$(u_clone_image "ubuntu")
-base=$(u_clone_ref "$img" latest)
-u_remove_refs_except "$img" "$base"
-
 # python 2
-py2=$(u_clone_ref "$base")
+py2=$(buildah from ubuntu)
 
-layer=$(u_open_layer "$py2")
-u_run "$layer" -- sh -c 'apt-get update && apt-get -y install python'
-py2_ver=$(u_run "$layer" -- python -V 2>&1 | cut -f 2 -d ' ')
-u_close_layer "$layer" "$py2" --mask-path /var/lib/apt/lists/
+buildah run $py2 sh -c 'apt-get update && apt-get -y install python'
+py2_ver=$(buildah run $py2 python -V 2>&1 | cut -f 2 -d ' ')
 
-u_config "$py2" --config.cmd /usr/bin/python
+buildah run $py2 sh -c "[ -d /var/lib/apt/lists ] && rm -rf /var/lib/apt/lists/*"
 
-u_write_ref "$py2" "$py2_ver"
+buildah config --cmd /usr/bin/python $py2
+
+img=$(buildah commit --rm $py2 python:"$py2_ver")
 # gets version in x.y form
-u_write_ref "$py2" "${py2_ver%.${py2_ver#*.*.}}"
-u_write_ref "$py2" 2
-u_remove_ref "$py2"
+buildah tag $img python:"${py2_ver%.${py2_ver#*.*.}}"
+buildah tag $img python:2
 
 # python 3
-py3=$(u_clone_ref "$base")
+py3=$(buildah from ubuntu)
 
-layer=$(u_open_layer "$py3")
-u_run "$layer" -- sh -c 'apt-get update && apt-get -y install python3'
-py3_ver=$(u_run "$layer" -- python3 -V 2>&1 | cut -f 2 -d ' ')
-u_close_layer "$layer" "$py3" --mask-path /var/lib/apt/lists/
+buildah run $py3 sh -c 'apt-get update && apt-get -y install python3'
+py3_ver=$(buildah run $py3 python3 -V 2>&1 | cut -f 2 -d ' ')
 
-u_config "$py3" --config.cmd /usr/bin/python3
+buildah run $py3 sh -c "[ -d /var/lib/apt/lists ] && rm -rf /var/lib/apt/lists/*"
 
-u_write_ref "$py3" "$py3_ver"
+buildah config --cmd /usr/bin/python3 $py3
+
+img=$(buildah commit --rm $py3 python)
+buildah tag $img python:"$py3_ver"
 # gets version in x.y form
-u_write_ref "$py3" "${py3_ver%.${py3_ver#*.*.}}"
-u_write_ref "$py3" 3
-u_write_ref "$py3" latest
-u_remove_ref "$py3"
-
-# cleanup
-u_remove_ref "$base"
-
-# save iamge
-img=$(u_set_image_name "$img" "python")
-u_clean_image "$img"
-u_serialize_image "$img" "python-all" > /dev/null
+buildah tag $img python:"${py3_ver%.${py3_ver#*.*.}}"
+buildah tag $img python:3
