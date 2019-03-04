688117e547e1f478903b2607184583d2e076b519
Reuben Lifshay <rlifshay@gmail.com>
2019-03-04 01:54:09 -0800

    COnvert nginx to use buildah.

--- nginx/cbuild.sh
+++ nginx/cbuild.sh
@@ -1,36 +1,21 @@
 #!/bin/sh
 set -e
 
-. ${UTILSDIR:=../cbutils/src}/umoci.lib.sh
-. ${UTILSDIR:=../cbutils/src}/runc.lib.sh
+ctr=$(buildah from ubuntu)
 
-u_set_loglevel $U_LOG_INFO
-u_set_autoclean
+buildah run $ctr sh -c 'apt-get update && DEBIAN_FRONTEND=noninteractive apt-get -y install nginx'
 
-# create image
-img=$(u_clone_image "ubuntu")
-work=$(u_clone_ref "$img" latest)
-u_remove_refs_except "$img" "$work"
+nginx_ver=$(expr match "$(buildah run $ctr nginx -v 2>&1)" '.*nginx/\([^ ]\+\)')
 
-layer=$(u_open_layer "$work")
+buildah run $ctr ln -sf /dev/stdout /var/log/nginx/access.log
+buildah run $ctr ln -sf /dev/stderr /var/log/nginx/error.log
 
-u_run "$layer" -- sh -c 'apt-get update && DEBIAN_FRONTEND=noninteractive apt-get -y install nginx'
-nginx_ver=$(expr match "$(u_run "$layer" -- nginx -v 2>&1)" '.*nginx/\([^ ]\+\)')
-u_log_eval ln -sf /dev/stdout $(u_layer_path "$layer" "/var/log/nginx/access.log")
-u_log_eval ln -sf /dev/stderr $(u_layer_path "$layer" "/var/log/nginx/error.log")
+buildah run $ctr sh -c "[ -d /var/lib/apt/lists ] && rm -rf /var/lib/apt/lists/*"
 
-u_close_layer "$layer" "$work" --mask-path /var/lib/apt/lists/
+buildah config \
+	--cmd "/usr/sbin/nginx -g 'daemon off;'" \
+	--port 80 \
+	$ctr
 
-u_config "$work" \
-	--config.cmd /usr/sbin/nginx --config.cmd -g --config.cmd 'daemon off;' \
-	--config.exposedports 80
-
-u_write_ref "$work" "$nginx_ver"
-u_write_ref "$work" latest
-
-u_remove_ref "$work"
-
-# save iamge
-img=$(u_set_image_name "$img" "nginx")
-u_clean_image "$img"
-u_serialize_image "$img" "nginx-${nginx_ver}" > /dev/null
+img=$(buildah commit --rm $ctr nginx)
+buildah tag $img nginx:"$nginx_ver"
